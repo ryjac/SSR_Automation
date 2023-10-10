04535508dfdb81b512f206a7c70e6169221c1284
dankewaffle <rynjacobs@gmail.com>
2023-10-10 11:44:22 -0700

    Switched from .txt to .xlsx for input/export.  Refactored/restructured code for modularity and better readability.  Added some more error handling.  Updated readme.

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 # SSR_Automation
 Python script that automates the task of individually running Medi-Cal Single Subscriber Response Eligibility checks, and saves the results to a file.
 
+# Packages to install
+    Requests
+    BeautifulSoup
+    Openpyxl
+    Dotenv
+- pip install requests bs4 openpyxl dotenv
+
 # Login with Medi-Cal Credentials
-  In **login_to_medi_cal()** replace *"enter_username"* and *"enter_password"* with your Medi-Cal Transaction Services username & password
-  ##
-    def login_to_medi_cal():
-      # Medi-Cal Credentials
-      username = "enter_username"
-      password = "enter_password"
+  Create a **.env** file and replace *"your_username_here"* & *"your_password_here"* with your Medi-Cal Transaction Services username & password
+
+    LOGIN_USERNAME=your_username_here
+    LOGIN_PASSWORD=your_password_here
 
 # Load Entry List
-  In **ssr.txt** add entries separated by a new line.
-  ##
-    Subscriber ID 1
-    DOB 1 (MM/DD/YYYY)
-    Issue Date 1 (MM/DD/YYYY)
-    Service Date 1 (MM/DD/YYYY)
+  In **subscriber_list.xlsx** add entries to be run in each row in the following format:
 
-    Subscriber ID 2
-    DOB 2 (MM/DD/YYYY)
-    Issue Date 2 (MM/DD/YYYY)
-    Service Date 2 (MM/DD/YYYY)
-
+    Subscriber ID 1    DOB 1 (MM/DD/YYYY)    Issue Date 1 (MM/DD/YYYY)    Service Date 1 (MM/DD/YYYY)
+    Subscriber ID 2    DOB 2 (MM/DD/YYYY)    Issue Date 2 (MM/DD/YYYY)    Service Date 2 (MM/DD/YYYY)
     ...
 
 # Run script
   Open a terminal and run the script with **py script.py**.
   
-  Results will be saved in **results.txt**.
+  Results will be saved in **Results.xlsx**.
--- script.py
+++ script.py
@@ -1,22 +1,59 @@
-import requests
 import os
+import requests
 from bs4 import BeautifulSoup
+import openpyxl
+from openpyxl.styles import Font
+from dotenv import load_dotenv
 
 # Define headers
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/91.0.4472.124 Safari/537.36"
 }
 
 # Start a session
 session = requests.Session()
+
+# Persist session
 session.headers.update(headers)
 
-def login_to_medi_cal():
-    # Medi-Cal Credentials
-    username = "enter_username"
-    password = "enter_password"
+# Globally initialize the workbook and worksheet at the start
+wb_results = openpyxl.Workbook()
+ws_results = wb_results.active
 
-    # URL
+# Create header row
+ws_results.append(["Subscriber Name", "Subscriber ID", "Birth Date", "Issue Date", "Service Date", "Primary Aid Code",
+                   "Responsible County", "Trace Number", "Eligibility Message"])
+
+# Apply bold font to header row
+bold_font = Font(bold=True)
+for cell in ws_results["1:1"]:
+    cell.font = bold_font
+
+
+def load_credentials():
+    # Load the .env file
+    load_dotenv()
+
+    # Access Medi-Cal credentials from the .env file
+    username = os.environ.get("LOGIN_USERNAME")
+    password = os.environ.get("LOGIN_PASSWORD")
+
+    # Check if username and password are None (not found in .env)
+    if username is None or password is None:
+        print("Error: .env file not found or missing LOGIN_USERNAME or LOGIN_PASSWORD.")
+        exit(1)  # You can choose to handle the error differently, e.g., raise an exception or exit the script
+    else:
+        print("Credentials loaded")
+
+    return username, password
+
+
+def login_to_medi_cal():
+    # Get credentials
+    username, password = load_credentials()
+
+    # Medi-Cal Login URL
     login_url = 'https://secure.medi-cal.ca.gov/mcwebpub/login.aspx'
 
     # Fetch the login page first
@@ -25,13 +62,37 @@ def login_to_medi_cal():
     # Use BeautifulSoup to parse the page
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract the necessary hidden form fields
+    # Get the login form data
+    form_data = get_login_form_data_from_soup(soup, username, password)
+
+    # POST the payload
+    login_response = session.post(login_url, data=form_data)
+
+    # Check the response to see if the login was successful
+    # Update this check based on the expected behavior of a successful login
+    if 'Single Subscriber' in login_response.text:
+        print('Medi-Cal login successful')
+
+        next_page_url = 'https://secure.medi-cal.ca.gov/eligibility/Eligibility.aspx'
+        response_next_page = session.get(next_page_url)
+
+        if 'Subscriber ID' in response_next_page.text:
+            print('Navigated to SSR Form')
+            print(f"--------------------------------\n")
+            submit_ssr()
+
+    else:
+        print('Medi-Cal login failed')
+
+
+def get_login_form_data_from_soup(soup, username, password):
+    # Extract hidden fields
     viewstate = soup.find('input', {'name': '__VIEWSTATE'})['value']
     viewstategenerator = soup.find('input', {'name': '__VIEWSTATEGENERATOR'})['value']
     eventvalidation = soup.find('input', {'name': '__EVENTVALIDATION'})['value']
 
-    # Create the payload using the extracted values and your actual credentials
-    form_data = {
+    # Create the payload using the extracted values and values from the entry
+    login_data = {
         '__VIEWSTATE': viewstate,
         '__VIEWSTATEGENERATOR': viewstategenerator,
         '__EVENTVALIDATION': eventvalidation,
@@ -40,173 +101,170 @@ def login_to_medi_cal():
         'ctl00$MainContent$btnSubmit': 'Login'
     }
 
-    # POST the payload
-    login_response = session.post(login_url, data=form_data)
+    return login_data
 
-    # Display status code
-    print("Status Code:", login_response.status_code)
 
-    # Check the response to see if the login was successful
-    # Update this check based on the expected behavior of a successful login
-    if 'Single Subscriber' in login_response.text:
-        print('Login successful!')
-        
-        next_page_url = 'https://secure.medi-cal.ca.gov/eligibility/Eligibility.aspx'
-        response_next_page = session.get(next_page_url)
-        
-        if 'Subscriber ID' in response_next_page.text:
-            print('Navigated to SSR Form')
-            if is_empty("ssr.txt"):
-                print("'ssr.txt' is empty.")
-            else:
-                submit_ssr()
-        
-        # print(response_next_page.text)
-    else:
-        print('Login failed!')
-        
-def is_empty(filename):
-    return os.path.getsize(filename) == 0
-        
-def get_next_entry(file):
+def get_next_entry(ws, current_row):
     """
     Get the next set of details from the file.
     Returns a dictionary with the details or None if the end of the file is reached.
     """
-    recip_id = file.readline().strip()
-    if not recip_id:  # End of file reached
+
+    recip_id = ws.cell(row=current_row, column=1).value
+    if not recip_id: # end of worksheet reached
         return None
 
-    recip_dob = file.readline().strip()
-    recip_doi = file.readline().strip()
-    recip_dos = file.readline().strip()
-    
-    # Skip the blank line
-    file.readline()
+    recip_dob = ws.cell(row=current_row, column=2).value
+    recip_doi = ws.cell(row=current_row, column=3).value
+    recip_dos = ws.cell(row=current_row, column=4).value
 
     return {
         "recip_id": recip_id,
         "recip_dob": recip_dob,
         "recip_doi": recip_doi,
         "recip_dos": recip_dos
-    }        
+    }
+
+
+def get_ssr_form_data_from_soup(soup, entry):
+    # Extract hidden fields
+    viewstate = soup.find('input', {'name': '__VIEWSTATE'})['value']
+    viewstategenerator = soup.find('input', {'name': '__VIEWSTATEGENERATOR'})['value']
+    eventvalidation = soup.find('input', {'name': '__EVENTVALIDATION'})['value']
+
+    # Create the payload using the extracted values and values from the entry
+    eligibility_data = {
+        '__VIEWSTATE': viewstate,
+        '__VIEWSTATEGENERATOR': viewstategenerator,
+        '__EVENTVALIDATION': eventvalidation,
+        'ctl00$MainContent$RecipID': entry["recip_id"],
+        'ctl00$MainContent$RecipDOB': entry["recip_dob"],
+        'ctl00$MainContent$RecipDOI': entry["recip_doi"],
+        'ctl00$MainContent$RecipDOS': entry["recip_dos"],
+        '__ASYNCPOST': 'true',
+        'ctl00$MainContent$Submit': 'Submit'
+    }
+
+    return eligibility_data
+
+
+def post_eligibility_data(data):
+    eligibility_page_url = 'https://secure.medi-cal.ca.gov/eligibility/Eligibility.aspx'
+    response_eligibility = session.post(eligibility_page_url, data=data)
+    return response_eligibility
+
+
+def parse_ssr_results(response):
+    # Parse results
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extracting the data with error handling
+
+    eligibility_message_elem = soup.find('span', {'id': 'MainContent_lblMessages'})
+    eligibility_message = eligibility_message_elem.text if eligibility_message_elem else "Not Found"
+
+    subscriber_name_elem = soup.find('div', {'id': 'MainContent_trname'})
+    subscriber_name = subscriber_name_elem.text.replace("Subscriber Name: ", "").strip() \
+        if subscriber_name_elem else "Not Found"
+
+    subscriber_id_elem = soup.find('div', {'id': 'MainContent_trssntrue'})
+    subscriber_id = subscriber_id_elem.text.replace("Subscriber ID: ", "").strip() \
+        if subscriber_id_elem else "Not Found"
+
+    birth_date_b_tag = soup.find('b', string="Subscriber Birth Date: ")
+    if birth_date_b_tag:
+        birth_date = birth_date_b_tag.next_sibling.strip()
+    else:
+        birth_date = "Not Found"
+
+    issue_date_b_tag = soup.find('b', string="Issue Date: ")
+    if issue_date_b_tag:
+        issue_date = issue_date_b_tag.next_sibling.strip()
+    else:
+        issue_date = "Not found"
+
+    primary_aid_code_b_tag = soup.find('b', string="Primary Aid Code: ")
+    if primary_aid_code_b_tag:
+        primary_aid_code = primary_aid_code_b_tag.next_sibling.strip()
+    else:
+        primary_aid_code = "Not found"
+
+    responsible_county_b_tag = soup.find('b', string="Responsible County: ")
+    if responsible_county_b_tag:
+        responsible_county = responsible_county_b_tag.next_sibling.strip()
+    else:
+        responsible_county = "Not found"
+
+    service_date_b_tag = soup.find('b', string="Service Date: ")
+    if service_date_b_tag:
+        service_date = service_date_b_tag.next_sibling.strip()
+    else:
+        service_date = "Not found"
+
+    trace_number_elem = soup.find('span', {'id': 'MainContent_lblEVCNo'})
+    trace_number = trace_number_elem.text if trace_number_elem else "Not Found"
+
+    return [subscriber_name, subscriber_id, birth_date, issue_date, service_date, primary_aid_code, responsible_county,
+            trace_number, eligibility_message]
+
+
+def append_results_to_excel(parsed_data):
+    global wb_results
+    global ws_results
+
+    # Append the parsed data to the worksheet and save
+    ws_results.append(parsed_data)
+    print(f"Adding SSR Result to spreadsheet\n")
+
+
+def get_total_entries(ws):
+    total = 0
+    for row in ws.iter_rows(min_row=2, min_col=1, max_col=1):  # Assuming "recip_id" is in the first column
+        if row[0].value:
+            total += 1
+    return total
+
 
 def submit_ssr():
-    # Read ssr to be ran from a file
-    with open('ssr.txt', 'r') as file:
-        while True:
-            entry = get_next_entry(file)
-            if entry is None:
-                break  # End of file reached, exit loop
-    
-            # Use BeautifulSoup to parse the page
-            eligibility_page_url = 'https://secure.medi-cal.ca.gov/eligibility/Eligibility.aspx'
-            response_eligibility = session.get(eligibility_page_url)
-            soup_eligibility = BeautifulSoup(response_eligibility.content, 'html.parser')
+    global wb_results
+    global ws_results
 
-            # Extract hidden fields
-            viewstate = soup_eligibility.find('input', {'name': '__VIEWSTATE'})['value']
-            viewstategenerator = soup_eligibility.find('input', {'name': '__VIEWSTATEGENERATOR'})['value']
-            eventvalidation = soup_eligibility.find('input', {'name': '__EVENTVALIDATION'})['value']
+    # Load the workbook and select the active worksheet
+    wb = openpyxl.load_workbook('subscriber_list.xlsx')
+    ws = wb.active
 
-            # Create the payload using the extracted values and values from the entry
-            eligibility_data = {
-                '__VIEWSTATE': viewstate,
-                '__VIEWSTATEGENERATOR': viewstategenerator,
-                '__EVENTVALIDATION': eventvalidation,
-                'ctl00$MainContent$RecipID': entry["recip_id"],
-                'ctl00$MainContent$RecipDOB': entry["recip_dob"],
-                'ctl00$MainContent$RecipDOI': entry["recip_doi"],
-                'ctl00$MainContent$RecipDOS': entry["recip_dos"],
-                '__ASYNCPOST': 'true',
-                'ctl00$MainContent$Submit': 'Submit'
-            }
+    current_row = 2  # assumes first row is header
+    total_entries = get_total_entries(ws)
 
-            eligibility_response = session.post(eligibility_page_url, data=eligibility_data)
+    while True:
+        entry = get_next_entry(ws, current_row)
+        if entry is None:
+            break  # End of worksheet reached, exit loop
 
-            # Display status code
-            print("Status Code:", eligibility_response.status_code)
-    
-            # if 'Single Subscriber Response' in eligibility_response.text:
-            print('Submit successful!')
-    
-            results_page_url = 'https://secure.medi-cal.ca.gov/Eligibility/EligResp.aspx'
-            results_next_page = session.get(results_page_url)
-    
-            if 'Single Subscriber Response' in results_next_page.text:
-                print('Obtained SSR Results:')
+        print(f"~~ Running entry ({current_row - 1} of {total_entries}) ~~")
 
-                # Parse results
-                soup = BeautifulSoup(results_next_page.text, 'html.parser')
-                
-                # Extracting the data with error handling
+        eligibility_page_url = 'https://secure.medi-cal.ca.gov/eligibility/Eligibility.aspx'
+        response_eligibility = session.get(eligibility_page_url)
+        soup_eligibility = BeautifulSoup(response_eligibility.content, 'html.parser')
 
-                eligibility_message_elem = soup.find('span', {'id': 'MainContent_lblMessages'})
-                eligibility_message = eligibility_message_elem.text if eligibility_message_elem else "Not Found"
+        form_data = get_ssr_form_data_from_soup(soup_eligibility, entry)
+        post_eligibility_data(form_data)
 
-                subscriber_name_elem = soup.find('div', {'id': 'MainContent_trname'})
-                subscriber_name = subscriber_name_elem.text.replace("Subscriber Name: ", "").strip() if subscriber_name_elem else "Not Found"
+        print('SSR Form submit successful')
 
-                subscriber_id_elem = soup.find('div', {'id': 'MainContent_trssntrue'})
-                subscriber_id = subscriber_id_elem.text.replace("Subscriber ID: ", "").strip() if subscriber_id_elem else "Not Found"
+        results_page_url = 'https://secure.medi-cal.ca.gov/Eligibility/EligResp.aspx'
+        response = session.get(results_page_url)
 
-                birth_date_b_tag = soup.find('b', string="Subscriber Birth Date: ")
-                if birth_date_b_tag:
-                    birth_date = birth_date_b_tag.next_sibling.strip()
-                else:
-                    birth_date = "Not Found"
-                    
-                issue_date_b_tag = soup.find('b', string="Issue Date: ")
-                if issue_date_b_tag:
-                    issue_date = issue_date_b_tag.next_sibling.strip()
-                else:
-                    issue_date = "Not found"
-                    
-                primary_aid_code_b_tag = soup.find('b', string="Primary Aid Code: ")
-                if primary_aid_code_b_tag:
-                    primary_aid_code = primary_aid_code_b_tag.next_sibling.strip()
-                else:
-                    primary_aid_code = "Not found"
+        if 'Single Subscriber Response' in response.text:
+            print('Obtained SSR Result')
+            parsed_data = parse_ssr_results(response)
+            append_results_to_excel(parsed_data)
+            current_row += 1  # Move to the next row for the next set of details
 
-                responsible_county_b_tag = soup.find('b', string="Responsible County: ")
-                if responsible_county_b_tag:
-                    responsible_county = responsible_county_b_tag.next_sibling.strip()
-                else:
-                    responsible_county = "Not found"
-                    
-                service_date_b_tag = soup.find('b', string="Service Date: ")
-                if service_date_b_tag:
-                    service_date = service_date_b_tag.next_sibling.strip()
-                else:
-                    service_date = "Not found"
+    wb_results.save('Results.xlsx')
+    print("--------------------------------")
+    print("Results saved in 'Results.xlsx'")
 
-                trace_number_elem = soup.find('span', {'id': 'MainContent_lblEVCNo'})
-                trace_number = trace_number_elem.text if trace_number_elem else "Not Found"
 
-                
-                # Print to console
-                print("Eligibility Message:", eligibility_message)
-                print("Subscriber Name:", subscriber_name)
-                print("Subscriber ID:", subscriber_id)
-                print("Birth Date:", birth_date)
-                print("Issue Date:", issue_date)
-                print("Primary Aid Code:", primary_aid_code)
-                print("Responsible County:", responsible_county)
-                print("Service Date:", service_date)
-                print("Trace Number:", trace_number)
-
-                # Save to results.txt
-                with open('results.txt', 'a') as f:
-                    f.write("Eligibility Message: " + eligibility_message + "\n")
-                    f.write("Subscriber Name: " + subscriber_name + "\n")
-                    f.write("Subscriber ID: " + subscriber_id + "\n")
-                    f.write("Birth Date: " + birth_date + "\n")
-                    f.write("Issue Date: " + issue_date + "\n")
-                    f.write("Primary Aid Code: " + primary_aid_code + "\n")
-                    f.write("Responsible County: " + responsible_county + "\n")
-                    f.write("Service Date: " + service_date + "\n")
-                    f.write("Trace Number: " + trace_number + "\n\n")
-                    
-                print("Results saved in 'Results.txt'")
-        
-login_to_medi_cal()
+if __name__ == '__main__':
+    login_to_medi_cal()
